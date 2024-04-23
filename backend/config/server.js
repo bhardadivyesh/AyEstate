59d921e2dbf8cdbe7d33af689662679d5f10575d
bhardadivyesh <bhardadivyesh9@gmail.com>
2024-04-23 13:03:47 +0530

    [db][change folder structure]

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9ba34e33920f1919bdf6aead714a26ad'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2c35a823-6a6c-444c-ada9-480a2ed1d169'
+};
